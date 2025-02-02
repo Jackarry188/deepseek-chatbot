35e738489296c1830fc79cce03341daf12ff8c6e
Jack <skyy_ok@126.com>
2025-02-02 19:08:07 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,9 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
+# export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
+export const APP_ID = '89126621-1d1e-4f8b-ae4c-266e41b7ce94' 
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
-export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+# export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const API_URL = 'http://103013yt7td85.vicp.fun/v1'
 export const APP_INFO: AppInfo = {
   title: 'DeepSeek ChatBot APP',
   description: '基于DeepSeek-R1:70B搭建的聊天机器人',
